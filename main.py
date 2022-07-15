88d535a5abcd704bccdb70dbb0807c3f9ff498e9
bendi24 <tass.bendeguz@gmail.com>
2022-07-15 19:15:34 +0200

    képek

--- main.py
+++ main.py
@@ -4,18 +4,58 @@ pygame.init()
 
 WIDTH = 500
 HEIGHT = 500
+szín = (0, 0, 0)
+SPEED = 10
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.clock()
 
-#PLayer class
+#Játékos class
 class Player:
+    def __init__(self, x, y, kép, speed=5, irány=0):
+        self.x = x
+        self.y = y
+        self.kép = kép
+        self.körvonal = self.kép.get_rect(center=(x, y))
+        self.speed = speed
+        self.irány = irány
+
+    def megjelenés(self):
+        display.blit(self.kép, self.körvonal)
+
+    def mozgás(self, keys):
+        if keys[pygame.K_w]:
+            self.y -= self.speed
+            self.irány = 0
+        elif keys[pygame.K_s]:
+            self.y += self.speed
+            self.irány = 1
+        if keys[pygame.K_d]:
+            self.x += self.speed
+            self.irány = 2
+        elif keys[pygame.K_a]:
+            self.x -= self.speed
+            self.irány = 3
+
+#Példányosítás
+player = Player(0, 0, pygame.image.load("walk_1.png").convert_alpha(), SPEED)
+háttér = Background(0, 0)
+
+#fő loop
+running = True
+billentyűk = pygame.key.get_pressed()
+while running:
+
+    háttér.megjelenés()
+    player.mozgás(billentyűk)
+    player.megjelenés()
 
 
-
-#main loop
-while True:
-
-    for event in pygame.event:
-        if event.type == pygame.quit():
+    #kilépés a játékból
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
             sys.exit()
-            pygame.Quit()
+
+    pygame.display.update()
+
